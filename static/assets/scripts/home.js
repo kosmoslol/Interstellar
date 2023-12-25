b0036a60baad1dcb9ab2d533d80d990cb453abdd
kosmos <119829358+kosmoslol@users.noreply.github.com>
2023-12-25 13:44:52 -0600

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -7,8 +7,8 @@ try {
 };
 
 if (!inFrame && !navigator.userAgent.includes('Firefox')) {
-    const popup = open('about:blank', '_blank')
-    if (!popup || popup.closed) alert('Please allow popups and redirects.');
+    const popup = open('about:blank')
+    if (!popup || popup.closed)
     else {
         popup.document.write(`
             <iframe src="${location.href}" style="position:fixed;top:0;left:0;width:100%;height:100%;outline:none;border:none;"></iframe>
